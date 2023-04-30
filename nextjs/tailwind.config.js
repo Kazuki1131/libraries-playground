b606046889f9423a9ad44b877b58fa04b4dccad4
Kazuki1131 <onetsubyo@gmail.com>
2023-04-30 16:54:33 +0900

    特定のCSSをプロジェクト全体で再利用できるよう調整

--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			backgroundColor: {
+				primary: '#1D3461',
+				success: '#5AB203',
+			},
+			textColor: {
+				primary: '#ffffff',
+				success: '#ffffff',
+				transparent: '#111111',
+			},
+			borderColor: {
+				transparent: 'black',
+			},
+			borderRadius: {
+				custom: '12px',
+			},
+			height: {
+				custom: '38px',
+			},
+			lineHeight: {
+				custom: '22px',
+			},
+		},
+	},
+	plugins: [],
+};
